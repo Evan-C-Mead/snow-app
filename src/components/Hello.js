f268af9907ed88063dcc02600aaadc428dd0c06a
Evan-C-Mead <evan.c.mead@gmail.com>
2023-01-17 22:07:47 -0600

    working on names array

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,6 +1,9 @@
 import React from "react";
 
 export default function Hello() {
+    // want to cycle through all names and say "HELLO XXXXX" instead of just saying "HELLO FARIEND"
+    // const names = ["Evan", "Jess", "Ray", "TJ", "Brantley", "Colleen", "Kasie", "Hunter", "Miz", "Christopher", "Amber", "Marvin", "Lakenya", "Taj", "Tony", "Pod #1"];
+
     return (
         <div className="hello">
             <h1 className="hello">HELLO FRIEND!</h1>
