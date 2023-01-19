7e5c3d3ca2625b66f102c7a50c6dff830f72b1e7
Evan-C-Mead <evan.c.mead@gmail.com>
2023-01-18 20:16:31 -0600

    state rename

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from "react";
 
 export default function Hello() {
     const names = ["Pod #1", "Ray", "TJ", "Brantley", "Colleen", "Kasie", "Hunter", "Miz", "Christopher", "Amber", "Marvin", "Lakenya", "Taj", "Tony", "Stephen", "Jeff", "Jess", "Evan", "Friends"];
-    const [helloName, setHelloName] = useState(names[0].toLocaleUpperCase());
+    const [name, setName] = useState(names[0].toLocaleUpperCase());
 
     useEffect(() => {
         const randomName = setInterval(() => {
             // gets random name from names[] every 3sec
             const randomize = names[Math.floor(Math.random() * names.length)].toLocaleUpperCase();
-            setHelloName(randomize);
+            setName(randomize);
             console.log(randomize);
         }, 5000);
         // clears the state of current rendered interval to allow new random name from names[]
@@ -37,7 +37,7 @@ export default function Hello() {
 
     return (
         <div className="hello">
-            <h1>{greeting.toLocaleUpperCase()} {helloName}!</h1>
+            <h1>{greeting.toLocaleUpperCase()} {name}!</h1>
         </div>
     )
 }
