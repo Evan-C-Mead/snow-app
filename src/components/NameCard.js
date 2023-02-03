e69be1062eda38f9e085dc433eba6fcd2bbd7383
Evan-C-Mead <evan.c.mead@gmail.com>
2023-02-03 16:55:33 -0600

    removed names from pod1 renamed array

--- src/components/NameCard.js
+++ src/components/NameCard.js
@@ -4,19 +4,19 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
 export default function NameCard() {
-    const names = ["Ray", "TJ", "Brantley", "Colleen", "Kasie", "Hunter", "Miz", "Christopher", "Amber", "Marvin", "Lakenya", "Taj", "Tony", "Stephen", "Jeff", "Jess", "Evan"].sort();
+    const pod1 = ["Ray", "TJ", "Brantley", "Colleen", "Kasie", "Hunter", "Miz", "Marvin", "Lakenya", "Taj", "Tony", "Stephen", "Jeff", "Jess", "Evan"].sort();
     // const [name, setName] = useState(names[0].toLocaleUpperCase());
     const nextGen = "NextGen Extern";
     
     return (
         <div className="name-cards">
             <Row xs={1} md={4} className="name-row g-4">
-                {Array.from({ length: names.length }).map((_, idx) => (
+                {Array.from({ length: pod1.length }).map((_, idx) => (
                     <Col className="name-col">
                         <Card className="name-card">
                             <Card.Img className="name-card-img" variant="top" src="/media/snow-logo.svg" />
                             <Card.Body className="name-card-body">
-                                <Card.Title className="name-card-title">{names[idx].toLocaleUpperCase()}</Card.Title>
+                                <Card.Title className="name-card-title">{pod1[idx].toLocaleUpperCase()}</Card.Title>
                                 <Card.Text className="name-card-text">{nextGen.toLocaleUpperCase()}</Card.Text>
                             </Card.Body>
                         </Card>
