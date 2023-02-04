8ebb8d30563774c15d6b87687ee4cee05eaa7f32
Evan-C-Mead <evan.c.mead@gmail.com>
2023-02-04 12:05:57 -0600

    key added to col array

--- src/components/NameCard.js
+++ src/components/NameCard.js
@@ -7,12 +7,12 @@ export default function NameCard() {
     const pod1 = ["Ray", "TJ", "Brantley", "Colleen", "Kasie", "Hunter", "Miz", "Marvin", "Lakenya", "Taj", "Tony", "Stephen", "Jeff", "Jess", "Evan"].sort();
     // const [name, setName] = useState(names[0].toLocaleUpperCase());
     const nextGen = "NextGen Extern";
-    
+
     return (
         <div className="name-cards">
             <Row xs={1} md={4} className="name-row g-4">
                 {Array.from({ length: pod1.length }).map((_, idx) => (
-                    <Col className="name-col">
+                    <Col className="name-col" key={idx}>
                         <Card className="name-card">
                             <Card.Img className="name-card-img" variant="top" src="/media/snow-logo.svg" />
                             <Card.Body className="name-card-body">
